72c707b682c06d8ed66d8d2efbc7d4d976b2889a
moldo <44651279+avrabie@users.noreply.github.com>
2026-03-09 22:40:19 +0100

    build image compatible for amd64

--- instructions.sh
+++ instructions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-docker build -t gluonstream/execodex-fe:latest .
+docker build --platform linux/amd64 -t gluonstream/execodex-fe:latest .
 docker push gluonstream/execodex-fe:latest
-kind load docker-image execodex-fe:latest --name s4v3
+kind load docker-image gluonstream/execodex-fe:latest --name s4v3
 kubectl apply -k k8s
 kubectl rollout restart deployment/execodex-fe -n execodex
